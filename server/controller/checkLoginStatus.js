876ea4f40749b8823856001c6c2603287222ef81
恒 <zhangheng001@zhongan.com>
2018-01-15 17:31:18 +0800

    update: login to Java

--- server/controller/checkLoginStatus.js
+++ server/controller/checkLoginStatus.js
@@ -1,11 +1,10 @@
 export default function (ctx, next) {
   if (ctx.session.sessionKey) {
-    next();
-  } else {
-    ctx.body = JSON.stringify({
-      success: false,
-      errorCode: 401,
-      errorMsg: '身份验证失败'
-    });
+    return next();
   }
+  ctx.body = JSON.stringify({
+    success: false,
+    errorCode: 401,
+    errorMsg: '身份验证失败'
+  });
 }
--- server/controller/writeAuthCookie.js
+++ server/controller/writeAuthCookie.js
@@ -7,4 +7,3 @@ export default async function (ctx, next) {
   }
   next();
 }
-
